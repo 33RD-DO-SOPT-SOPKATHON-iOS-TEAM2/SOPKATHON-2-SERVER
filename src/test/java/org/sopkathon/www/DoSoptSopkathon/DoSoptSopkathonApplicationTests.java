a72497558a2a7c35167eb4729cb60dcd915fe1f0
sunseo18 <sunseo1818@gmail.com>
2023-11-25 01:54:39 +0900

    [INIT] 프로젝트 초기세팅

---
+++ src/test/java/org/sopkathon/www/DoSoptSopkathon/DoSoptSopkathonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopkathon.www.DoSoptSopkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoSoptSopkathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
