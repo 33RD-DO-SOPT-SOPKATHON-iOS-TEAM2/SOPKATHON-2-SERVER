798ecdec8da9a4ffc0f6732ea4c24f8cac80eff2
sunseo18 <sunseo1818@gmail.com>
2023-11-26 01:49:33 +0900

    [ADD] User조회하고 없으면 익셉션 발생하는 로직 작성 #4

--- src/main/java/org/sopkathon/www/DoSoptSopkathon/user/exception/UserException.java
+++ src/main/java/org/sopkathon/www/DoSoptSopkathon/user/exception/UserException.java
@@ -1,2 +1,10 @@
-package org.sopkathon.www.DoSoptSopkathon.user.exception;public class UserException {
+package org.sopkathon.www.DoSoptSopkathon.user.exception;
+
+import org.sopkathon.www.DoSoptSopkathon.exception.BaseCustomException;
+import org.sopkathon.www.DoSoptSopkathon.exception.ExceptionType;
+
+public class UserException extends BaseCustomException {
+    public UserException(ExceptionType exceptionType) {
+        super(exceptionType);
+    }
 }
--- src/main/java/org/sopkathon/www/DoSoptSopkathon/user/exception/UserExceptionType.java
+++ src/main/java/org/sopkathon/www/DoSoptSopkathon/user/exception/UserExceptionType.java
@@ -1,2 +1,23 @@
-package org.sopkathon.www.DoSoptSopkathon.user.exception;public enum UserExceptionType {
+package org.sopkathon.www.DoSoptSopkathon.user.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.sopkathon.www.DoSoptSopkathon.exception.ExceptionType;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum UserExceptionType implements ExceptionType {
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus();
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
 }
