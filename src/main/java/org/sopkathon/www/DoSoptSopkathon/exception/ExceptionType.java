858da94c383b40e42482cdce2149abd45bbbb572
sunseo18 <sunseo1818@gmail.com>
2023-11-26 00:33:24 +0900

    [FEAT] 익셉션 처리 로직을 구현한다 #6

---
+++ src/main/java/org/sopkathon/www/DoSoptSopkathon/exception/ExceptionType.java
@@ -0,0 +1,12 @@
+package org.sopkathon.www.DoSoptSopkathon.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionType {
+
+    String name();
+
+    HttpStatus httpStatus();
+
+    String message();
+}
