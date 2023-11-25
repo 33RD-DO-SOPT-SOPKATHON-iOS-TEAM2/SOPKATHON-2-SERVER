fb4c89904eac90d5a190b024fb4c1aeda467d6a6
sunseo18 <sunseo1818@gmail.com>
2023-11-26 04:34:03 +0900

    [ADD] 이미 증서를 등록한 경우 다시 등록하지 못하는 로직 추가 #17

--- src/main/java/org/sopkathon/www/DoSoptSopkathon/card/exception/CardExceptionType.java
+++ src/main/java/org/sopkathon/www/DoSoptSopkathon/card/exception/CardExceptionType.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum CardExceptionType implements ExceptionType {
-    NOT_FOUND_CARD(HttpStatus.NOT_FOUND, "증서가 존재하지 않습니다")
-    ;
+    NOT_FOUND_CARD(HttpStatus.NOT_FOUND, "증서가 존재하지 않습니다"),
+    ALREADY_CREATED_CARD(HttpStatus.CONFLICT, "이미 증서를 등록했습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
